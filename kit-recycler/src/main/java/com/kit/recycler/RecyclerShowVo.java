97975b5c9aa5d5e063f286727dc43a9e8f399687
liuqiang1 <liuqiang1@evergrande.cn>
2018-08-03 09:26:29 +0800

    特性修改

--- kit-recycler/src/main/java/com/kit/recycler/RecyclerShowVo.java
+++ kit-recycler/src/main/java/com/kit/recycler/RecyclerShowVo.java
@@ -6,15 +6,24 @@ public class RecyclerShowVo {
 
     public String id;
     public String url;
-    public String info_1;
-    public String info_2;
-    public String info_3;
-    public String info_4;
-    public String info_5;
-    public String info_6;
-    public String info_7;
-    public String info_8;
-    public String info_9;
+
+    public String section_1_1;
+    public String section_1_2;
+    public String section_1_3;
+    public String section_1_4;
+    public String section_1_5;
+
+    public String section_2_1;
+    public String section_2_2;
+    public String section_2_3;
+    public String section_2_4;
+    public String section_2_5;
+
+    public String section_3_1;
+    public String section_3_2;
+    public String section_3_3;
+    public String section_3_4;
+    public String section_3_5;
 
     public String time_1;
     public String time_2;
